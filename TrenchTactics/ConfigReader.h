781ed3291ed9ff045d891178aa68152862c38848
Zeitler, Moritz <moritz.zeitler@student.fhws.de>
2020-04-13 21:13:25 +0200

    #30 initial commit, added config files as well as empty headers and a small piece of sample code

---
+++ TrenchTactics/ConfigReader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../jsoncpp/dist/json/json.h"
+
+class ConfigReader
+{
+public:
+	~ConfigReader();
+	ConfigReader(const ConfigReader&) = delete;
+	ConfigReader& operator=(const ConfigReader&) = delete;
+	ConfigReader(ConfigReader&&) = delete;
+	ConfigReader& operator=(ConfigReader&&) = delete;
+
+	static auto& instance() {
+		static ConfigReader confReader;
+		return ConfigReader;
+	}
+private:
+	ConfigReader();
+};
