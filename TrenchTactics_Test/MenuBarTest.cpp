da519ba45050b217744caeb4cc579c87754cbc6f
Zeitler, Moritz <moritz.zeitler@student.fhws.de>
2020-06-13 09:32:42 +0200

    starting to add more tests - problems with fonts dunno why - gonna move Tests into main project soon

---
+++ TrenchTactics_Test/MenuBarTest.cpp
@@ -0,0 +1,23 @@
+#include "catch.hpp"
+#include "../TrenchTactics/MenuBar.h"
+
+TEST_CASE("Test init functions") {
+	std::shared_ptr<MenuBar> testBar = make_shared<MenuBar>();
+
+}
+
+TEST_CASE("") {
+
+}
+
+TEST_CASE("") {
+
+}
+
+TEST_CASE("") {
+
+}
+
+TEST_CASE("") {
+
+}
