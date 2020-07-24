71b32ca191ca08980a83f05199b96c2d9649f466
lennard Rose <lennardrose@web.de>
2020-07-24 19:49:18 +0200

    menubar tests

--- TrenchTactics_Test/MenuBarTest.cpp
+++ TrenchTactics_Test/MenuBarTest.cpp
@@ -1,23 +1,31 @@
 #include "catch.hpp"
 #include "../TrenchTactics/MenuBar.h"
 
+using namespace std;
+
 TEST_CASE("Test init functions") {
-	//std::shared_ptr<MenuBar> testBar = make_shared<MenuBar>();
+	shared_ptr<MenuBar> testBar = make_shared<MenuBar>();
+	testBar->init();
 
+	REQUIRE(testBar != nullptr);
 }
 
-TEST_CASE("") {
+TEST_CASE("Test if Buttons are created") {
+	shared_ptr<MenuBar> testBar = make_shared<MenuBar>();
+	testBar->init();
+	testBar->initButtons(GAMEPHASES::GAMEPHASE::BUY);
 
+	REQUIRE(testBar->getMenuBar().get()->at(7).at(1).get()->getButton() != nullptr);
 }
 
-TEST_CASE("") {
+TEST_CASE("Test if Buttons are deleted") {
+	shared_ptr<MenuBar> testBar = make_shared<MenuBar>();
+	testBar->init();
+	testBar->initButtons(GAMEPHASES::GAMEPHASE::BUY);
+	testBar->deleteAllButtons();
 
+	for (int i = 4; i < 15; i = i++) {
+		REQUIRE(testBar->getMenuBar().get()->at(i).at(1).get()->getButton() == nullptr);
+	}
 }
 
-TEST_CASE("") {
-
-}
-
-TEST_CASE("") {
-
-}
